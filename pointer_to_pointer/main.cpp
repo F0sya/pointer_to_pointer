e84af5673b4287d28bd299a48f5fb31ad6a58c58
F0sya <nikitachmyria@gmail.com>
2024-06-20 19:26:08 +0300

    First practice exercise is done.

--- pointer_to_pointer/main.cpp
+++ pointer_to_pointer/main.cpp
@@ -1,6 +1,62 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 using namespace std;
 
-int main() {
-	return 0;
+template<typename T>
+T** createMatrix(int max_row, int max_col)
+{
+    T** arr = new T * [max_row];
+    for (int i = 0; i < max_row; i++)
+    {
+        arr[i] = new T[max_col];
+    }
+    return arr;
+}
+
+template<typename T>
+void fillMatrix(T** arr, int row, int col)
+{
+    for (size_t i = 0; i < row; i++)
+    {
+        for (size_t j = 0; j < col; j++)
+        {
+            arr[i][j] = rand() % 10;
+        }
+    }
+}
+
+template<typename T>
+void printMatrix(T** arr, int row, int col)
+{
+    for (size_t i = 0; i < row; i++)
+    {
+        for (size_t j = 0; j < col; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+template<typename T>
+T** addToMatrix(T** arr, int row, int col) {
+    for (size_t i = 0; i < col; i++) {
+        arr[row][i] = rand() % 10;
+    }
+    return arr;
+}
+
+int main()
+{
+    int max_col = 10;
+    int max_row = 10;
+    int col; cout << "Enter an amount of columns:"; cin >> col;
+    int row; cout << "Enter an amount of rows:"; cin >> row;
+    int** arr = createMatrix<int>(max_row, max_col);
+    fillMatrix(arr, row, col);
+    printMatrix(arr, row, col);
+    cout << endl << "Matrix after adding new row:" << endl;
+    arr = addToMatrix(arr, row, col);
+    printMatrix(arr, row + 1, col);
+    return 0;
 }
