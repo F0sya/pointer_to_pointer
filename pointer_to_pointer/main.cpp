27c296531735759feef75cc33d134921a2b2b7da
F0sya <nikitachmyria@gmail.com>
2024-06-24 20:57:04 +0300

    Third practice exercise is done.

--- pointer_to_pointer/main.cpp
+++ pointer_to_pointer/main.cpp
@@ -1,6 +1,77 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <ctime>
 using namespace std;
 
-int main() {
-	return 0;
+template<typename T>
+T** createMatrix(int max_row, int max_col)
+{
+    T** arr = new T * [max_row];
+    for (int i = 0; i < max_row; i++)
+    {
+        arr[i] = new T[max_col];
+    }
+    return arr;
+}
+
+template<typename T>
+void fillMatrix(T** arr, int row, int col)
+{
+    for (size_t i = 0; i < row; i++)
+    {
+        for (size_t j = 0; j < col; j++)
+        {
+            arr[i][j] = rand() % 10;
+        }
+    }
+}
+
+template<typename T>
+void printMatrix(T** arr, int row, int col)
+{
+    for (size_t i = 0; i < row; i++)
+    {
+        for (size_t j = 0; j < col; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+template<typename T>
+T** addToMatrix(T** arr, int row, int col, int choice) {
+    int** newArray = new int* [row + 1];
+
+    for (int i = 0, j = 0; i <= row; i++) {
+        if (i == choice) {
+            newArray[i] = new int[col];
+            for (int k = 0; k < col; k++) {
+                newArray[i][k] = rand() % 10;
+            }
+        }
+        else {
+            newArray[i] = arr[j++];
+        }
+    }
+    return newArray;
+}
+
+int main()
+{
+    srand(time(0));
+    int max_col = 10;
+    int max_row = 10;
+    int col; cout << "Enter an amount of columns:"; cin >> col;
+    int row; cout << "Enter an amount of rows:"; cin >> row;
+    int choice; cout << "Enter an index of line:"; cin >> choice;
+    int** arr = createMatrix<int>(max_row, max_col);
+    fillMatrix(arr, row, col);
+    printMatrix(arr, row, col);
+    cout << endl << "Matrix after adding new row:" << endl;
+    arr = addToMatrix(arr, row, col,choice);
+    printMatrix(arr, row + 1, col);
+
+    delete[] arr;
+    return 0;
 }
